bc85ba3d5d283e911762644e44e4583a4c1b4bf0
Kachit <antoxachaos@gmail.com>
2017-11-15 14:45:59 +0300

    Add directory config loader

---
+++ src/Kachit/Config/Writer/Directory.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Class Directory
+ *
+ * @package Orbitum\Billing\Common\Config\Loader
+ * @author Kachit
+ */
+namespace Kachit\Config\Writer;
+
+use Kachit\Config\ConfigInterface;
+use Kachit\Config\WriterInterface;
+use FilesystemIterator;
+use SplFileInfo;
+
+class Directory implements WriterInterface
+{
+    /**
+     * @var WriterInterface
+     */
+    private $writer;
+
+    /**
+     * DirectoryLoader constructor
+     *
+     * @param WriterInterface $writer
+     */
+    public function __construct(WriterInterface $writer)
+    {
+        $this->writer = $writer;
+    }
+
+    /**
+     * @param ConfigInterface $config
+     * @param string|null $path
+     * @return bool
+     */
+    public function write(ConfigInterface $config, string $path = null): bool
+    {
+        return true;
+    }
+}
